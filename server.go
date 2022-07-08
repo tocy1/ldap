6ad4eac1f2a324812d12efd229f86bae8614365d
Nwoko Tochukwu, SUB-SDC-NL-DOS-ECD <Tochukwu.Nwoko@swisscom.com>
2022-07-08 10:55:37 +0200

    Print request data

--- server.go
+++ server.go
@@ -443,18 +443,18 @@ func encodeWhoAmIResponse(result string, messageID uint64, responseType uint8, l
 	response.AppendChild(ber.NewInteger(ber.ClassUniversal, ber.TypePrimitive, ber.TagEnumerated, uint64(ldapResultCode), "resultCode: "))
 	response.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, "", "matchedDN: "))
 	//response.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, message, "errorMessage: "))
-	response.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, "", "errorMessage: "))
+	//response.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, "", "errorMessage: "))
 	response.AppendChild(ber.NewString(ber.ClassUniversal, ber.TypePrimitive, ber.TagOctetString, result, ""))
-	// whoAmIResponse := ber.Encode(ber.ClassUniversal, ber.TypePrimitive, ber.TagEmbeddedPDV, result, "")
+	whoAmIResponse := ber.Encode(ber.ClassUniversal, ber.TypePrimitive, ber.TagEmbeddedPDV, "", "Attributes:")
 
 	identity := &ber.Packet{}
 	identity.Data = new(bytes.Buffer)
 	identity.Data.Write([]byte(result))
 	identity.Value = result
 	identity.Raw = true
-	// whoAmIResponse.AppendChild(identity)
+	whoAmIResponse.AppendChild(identity)
 
-	response.AppendChild(identity)
+	response.AppendChild(whoAmIResponse)
 	responsePacket.AppendChild(response)
 	return responsePacket
 }
